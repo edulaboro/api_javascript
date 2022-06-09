107a4eaef05b52513a419d3c7b4d20c6da9cb886
Eduardo Ferreira <esferreira.tecno@gmail.com>
2022-06-09 20:53:46 -0300

    Implementando visualização da api no HTML

--- nativo/script.js
+++ nativo/script.js
@@ -1,3 +1,5 @@
+let tabela = createElement("#tabela")
+//let lista = document.querySelector("#lista")
 // ASYNC informa que a função é assincrona
 async function carregarDados(){
     const url = "https://swapi.dev/api/people/?format=json"
@@ -11,7 +13,16 @@ async function carregarDados(){
         
         // ELEMENTOS é uma nomeclatura gerada pelo desenvolvedor e NAME é um elemento da API
         for (elementos of dados.results) {
-            console.log ('Sou ${elementos.name} nasci no ano ${elementos.birth_year}')
+            //console.log (`Sou ${elementos.name} nasci no ano ${elementos.birth_year}`)
+            console.log(elementos)
+            //Criando elementos html dinamicamente
+            /*
+            const itemLista= document.createElement("li")
+            itemLista.classList.add("list-group-item")
+            itemLista.textContent = `Meu nome é: ${elementos.name} ----- Gênero:${elementos.gender} ---- Altura:${elementos.height}`
+
+            lista.appendChild(itemLista)*/
+            document.getElementById("tabela").oppenChild(novaTabela)
         }
 
         //console.log (dados.results)
